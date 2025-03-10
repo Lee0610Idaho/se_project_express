bd29bb0976dda07e7e28469310063b30e29bebba
Jackie Lee <jackieleeid2000@gmail.com>
2025-03-10 14:50:44 -0600

    Routes being implemented, validator installed

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const mainRouter = require("./routes/index");
+
 const app = express();
-
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
-
 const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+app.use("/", mainRouter);
+
 app.listen(3001, () => {
   console.log(`Listening on port ${PORT}`);
 });
