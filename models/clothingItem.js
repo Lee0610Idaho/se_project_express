329e9c036387a45e2ff53e2ce80a666d71078cfe
Jackie Lee <jackieleeid2000@gmail.com>
2025-03-12 16:49:03 -0600

    Implementing error status codes as constants

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -5,18 +5,38 @@ const clothingItem = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   weather: {
     type: String,
     required: true,
+    enum: ["hot", "warm", "cold"],
   },
   imageURL: {
     type: String,
     required: true,
     validate: {
       validator: (v) => validator.isURL(v),
-      message: "Link is not Valid",
+      message: "Input is not valid",
     },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      default: "",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
   },
 });
 
