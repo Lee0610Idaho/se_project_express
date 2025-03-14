6d5fc86ca21a635ef23e63eeb3ef77f611c730b9
Jackie Lee <jackieleeid2000@gmail.com>
2025-03-14 12:45:38 -0600

    removed/commented out console statements

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const { PORT = 3001 } = process.env;
 mongoose
   .connect("mongodb://localhost:27017/wtwr_db")
   .then(() => {
-    console.log("Connected to DB");
+    // console.log("Connected to DB");
   })
-  .catch(console.error);
+  .catch();
 
 app.use((req, res, next) => {
   req.user = {
@@ -23,6 +23,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(PORT, () => {
+  // console.log(`Listening on port ${PORT}`);
 });
